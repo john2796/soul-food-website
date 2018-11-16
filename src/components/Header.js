59360255c89cc1cbe95d2f1fc42f2a8276aa0fd3
john2796 <benedictmiranda27@yahoo.com>
2018-11-15 20:51:59 -0800

    setting up project

---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components'
+import Wrapper from '../Styles/Wrapper';
+
+// style Starts here
+const HeaderMain = styled.header`
+  /* background: rgba(12,12,13,0.78); */
+  background: black;
+ .wrapper {
+  display: flex;
+  justify-content:space-between;
+  align-items: center;
+  padding: 30px 0;
+ }
+ nav a ,div a{ 
+   color: white;
+   text-decoration: none;
+ }
+ nav { 
+   display: flex;
+   width: 48%;
+   justify-content: space-between;
+ }
+`
+// style Ends here
+
+const Header = (props) => {
+  return (
+    <>
+      <HeaderMain>
+        <Wrapper className="wrapper">
+          <div><a href="..">Logo</a></div>
+          <nav>
+            <a href="..">Home</a>
+            <a href="..">About</a>
+            <a href="..">Menu</a>
+            <a href="..">Gallery</a>
+            <a href="..">Reservations</a>
+            <a href="..">Location</a>
+          </nav>
+        </Wrapper>
+      </HeaderMain>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Header from '../Header';
+
+
+
+class Layout extends Component {
+  state = {}
+  render() {
+    return (
+      <>
+        <Header />
+      </>
+    );
+  }
+}
+
+export default Layout;
